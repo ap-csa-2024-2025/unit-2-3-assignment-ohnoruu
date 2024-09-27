a2c8ea24fffda2d8a27b2a4cc2d7d1b4a2b8e782
ohnoruu <rubydao007@gmail.com>
2024-09-27 10:52:29 -0700

    jrfiofjorijegijtiguwth

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,34 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("dictionary", 3));
+    System.out.println(delEnd("library", 5));
+    System.out.println(compareLower("antelope", "tiger"));
   }
 
-  // write solutions to problems below
+  public static String firstLastN(String word, int n){
+    String output = new String();
+    //Getting first n letters
+    String firstN = word.substring(0,n); //0-(n-1)
+    //Getting last n letters
+    int indexOfNToLast = word.length() - n; //index of nth to last letter
+    String lastN = word.substring(indexOfNToLast);
+    output = firstN+lastN;
+    return output;
+  }
+
+  public static String delEnd(String word, int n){
+    int wordLength = word.length();
+    String output = word.substring(0,(wordLength-n));
+    return output;
+  }
+
+  public static int compareLower(String word1, String word2){
+    //int compareTo(String other)
+    String lowerWord1 = word1.toLowerCase();
+    String lowerWord2 = word2.toLowerCase();
+    int output = lowerWord1.compareTo(lowerWord2);
+    return output;
+  }
 
 }
